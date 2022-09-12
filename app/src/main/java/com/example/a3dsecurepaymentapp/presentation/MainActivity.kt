593bcfdb3434ffa54f39b8f1830c0ebfad66b2c7
Harsh Gaba <harshgaba08@gmail.com>
2022-09-12 21:33:03 +0800

    Project structure setup
     setting up project structure

---
+++ app/src/main/java/com/example/a3dsecurepaymentapp/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a3dsecurepaymentapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
