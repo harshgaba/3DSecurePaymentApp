6534de24b4d1367333741f00423e91b85e166fa1
Harsh Gaba <harshgaba08@gmail.com>
2022-09-15 05:40:09 +0800

    Dinner credit card format handling use case updated

---
+++ app/src/main/java/com/example/a3dsecurepaymentapp/domain/use_case/card_details/formaters/FormatDinnersClubCard.kt
@@ -0,0 +1,46 @@
+package com.example.a3dsecurepaymentapp.domain.use_case.card_details.formaters
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import javax.inject.Inject
+
+class FormatDinnersClubCard @Inject constructor(){
+    operator fun invoke(text: AnnotatedString): TransformedText {
+
+        // Making XXXX-XXXXXX-XXXX card number
+        val trimmed = if (text.text.length >= 14) text.text.substring(0..13) else text.text
+        var out = ""
+
+        for (i in trimmed.indices) {
+            out += trimmed[i]
+            if (i ==3 || i == 9 && i != 13) out += "-"
+        }
+
+        /**
+         * The offset translator should ignore the hyphen characters, so conversion from
+         *  original offset to transformed text works like
+         *  - The 3th char of the original text is 4th char in the transformed text.
+         *  - The 14th char of the original text is 16th char in the transformed text.
+         *  Similarly, the reverse conversion works like
+         *  - The 4th char of the transformed text is 3th char in the original text.
+         *  - The 16th char of the transformed text is 14th char in the original text.
+         */
+        val creditCardOffsetTranslator = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                if (offset <= 3) return offset
+                if (offset <= 9) return offset + 1
+                if(offset <= 14) return offset + 2
+                return 16
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                if (offset <= 4) return offset
+                if (offset <= 11) return offset - 1
+                if(offset <= 16) return offset - 2
+                return 14
+            }
+        }
+        return TransformedText(AnnotatedString(out), creditCardOffsetTranslator)
+    }
+}
