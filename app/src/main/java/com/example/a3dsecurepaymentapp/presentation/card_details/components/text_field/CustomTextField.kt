89567c8f1f447ff5403dfb296a56c689ac37005b
Harsh Gaba <harshgaba08@gmail.com>
2022-09-15 05:40:51 +0800

    Custom input text field added for Credit card and CVV

---
+++ app/src/main/java/com/example/a3dsecurepaymentapp/presentation/card_details/components/text_field/CustomTextField.kt
@@ -0,0 +1,64 @@
+package com.example.a3dsecurepaymentapp.presentation.card_details.components.text_field
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier,
+    inputWrapper: InputWrapper,
+    @StringRes labelResId: Int,
+    keyboardOptions: KeyboardOptions = remember {
+        KeyboardOptions.Default
+    },
+    visualTransformation: VisualTransformation = remember {
+        VisualTransformation.None
+    },
+    onValueChange: OnValueChange,
+    onImeKeyAction: OnImeKeyAction
+
+) {
+    val fieldValue = remember {
+        mutableStateOf(TextFieldValue(inputWrapper.value, TextRange(inputWrapper.value.length)))
+    }
+    Column {
+        TextField(
+            modifier = modifier,
+            value = fieldValue.value,
+            onValueChange = {
+                fieldValue.value = it
+                onValueChange(it.text)
+            },
+            label = { Text(stringResource(labelResId)) },
+            isError = inputWrapper.errorId != null,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = remember {
+                KeyboardActions(onAny = { onImeKeyAction() })
+            },
+        )
+        if (inputWrapper.errorId != null) {
+            Text(
+                text = stringResource(inputWrapper.errorId),
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+        }
+    }
+}
